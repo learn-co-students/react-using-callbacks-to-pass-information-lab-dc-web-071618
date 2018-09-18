66e59aa0d6886b76ac232259a989c0ca0eb4992e
julianajlk <julianajlk@gmail.com>
2018-09-18 13:38:19 -0400

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,36 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      colorSelected: '#fff'
+    }
   }
 
+  handleColorSelected = (color) => {
+    //cannot console.log insite setState. color is the color that was selected on click. It is being stored on state(use react debugger, will show you the props and state of this compoment)
+    console.log(color)
+    this.setState({
+      colorSelected: color
+    })
+  }
+
+  //this will pass the colorSelected to Cell Component
+  passColorSelected = () => this.state.colorSelected
+
+  //added passColorSelected as props 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} passColorSelected={this.passColorSelected}/>)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
-
+//added handleColorSelected as a prop to Matrix(orange)
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector colorSelected={this.colorSelected} handleColorSelected={this.handleColorSelected} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
